a603a95e685b68aa21ef7ed160cc3022ed6609d2
Tharwin <tharwin.carr08@gmail.com>
2020-09-27 23:21:09 -0700

    bug fixes

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(cors())
 app.use(function validateBearerToken(req, res, next) {
     const apiToken= process.env.API_TOKEN
     const authToken= req.get('Authorization')
-
     if(!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized Request' })
     }
